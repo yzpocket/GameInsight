d462b36b2cbdaa0ec758203f71057487e94f68cf
INYONG KIM <ohnyong87@gmail.com>
2023-08-09 22:36:40 +0900

    feature : Add user register and login function with Hashlib
    * 회원 가입 및 암호화, DB insert
    - POST 방식을 통해 회원가입 폼으로부터 데이터 요청
    -- 이름, 아이디, 비밀번호, 주소 등 정보 변수 선언 및 할당
    -- 비밀번호는 Hashlib 라이브러리를 통한 sha256 방법 암호화
    -- DB로 정보 전달 중 Radio 밸류값을 못 얻는 오류 해결 중
    *  로그인 기능 구현 진행 흐름 파악
    - session을 통한 로그인 상태 유지, 만료 부분 작성 중

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,31 @@
 $(document).ready(function () {
-
 });
+
+function join(){
+    let user_name = $('#user_name').val()
+    let user_id = $('#user_id').val()
+    let user_password1 = $('#user_password1').val()
+    let user_password2 = $('#user_password2').val() // 유효성 체크용
+    let user_postcode = $('#user_postcode').val()
+    let user_address1 = $('#user_address1').val()
+    let user_address2 = $('#user_address2').val()
+    let user_email = $('#user_email').val()
+    let user_type = $('#user_type').val()
+    console.log(user_type);
+
+    let formData = new FormData();
+    formData.append("user_name_give", user_name)
+    formData.append("user_id_give", user_id)
+    formData.append("user_password1_give", user_password1)
+    formData.append("user_postcode_give", user_postcode)
+    formData.append("user_address1_give", user_address1)
+    formData.append("user_address2_give", user_address2)
+    formData.append("user_email_give", user_email)
+    formData.append("user_type_give", user_type)
+
+    fetch('/join', { method: "POST", body: formData, }).then((response) => response.json()).then((data) => {
+        alert(data["msg"]);
+        // 브라우저 새로고침 추가
+        window.location.reload();
+    });
+}
