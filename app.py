440c414740154f744480671a6ff31fdbd6239373
yame123 <inyasang@gmail.com>
2023-08-10 21:46:26 +0900

    feature : Add posting critic's review, unfolding omitted review, refolding review
    * 평론가 리뷰 구현
    
    - app.py
    -- 평론가 리뷰 READ를 위한 GET api 추가 구현
    -- url 유효성 평가 수정 및 저장용 일부 항목(no, commenter, upload time) 추가
    
    - static/js/script.js
    -- 더보기 버튼을 통해 모든 리뷰를 볼 수 있도록 함수 구현
    -- 닫기 버튼을 통해 기존 상태로 돌아가도록 함수 구현
    -- 평론가 리뷰 입력을 위한 function 구현
    
    - static/css/style.css
    -- 평론가 리뷰 작성 페이지 style 구현
    
    - templates/index.html
    -- user type이 평론가일 경우 평론 입력 항목 추가를 위해 nav_menu에 id 설정
    
    - 이후 추가해야할 항목
    -- 평론가 리뷰 입력이 가능한 계정 확인 및 입력 시 id 등록을 위한 session 불러오기 기능 확인
    -- 평론가 리뷰 등록 시 user의 이름을 저장하기 위해 호출하는 function 구현

--- app.py
+++ app.py
@@ -3,7 +3,9 @@
 # view페이지 렌더링을 위한 render_template 메서드
 # 요청 데이터에 접근 할 수 있는 flask.request 모듈
 # dictionary를 json형식의 응답 데이터를 내보낼 수 있는 jsonify 메서드
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify,session
+import time
+wday = ['월', '화', '수', '목', '금', '토', '일']
 app = Flask(__name__)
 
 # MongoDB사용을 위한 pymongo와 certifi 임포트
@@ -51,27 +53,39 @@ def game_post():
    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
    data = requests.get(url_receive,headers=headers)
    soup = BeautifulSoup(data.text, 'html.parser')
+   search_url=soup.select_one('meta[property="og:url"]').find('https://www.gamemeca.com/game.php?rts=gmview&')
    
-   ogtitle=soup.select_one('meta[property="og:title"]')['content'].split(" - ")[0]
+   if search_url == -1:
+      ogtitle=''
+   else:
+      ogtitle = soup.select_one('meta[property="og:title"]')['content'].split(" - ")[0]
+      
    if ogtitle!='':
       ogimage=soup.select_one('meta[property="og:image"]')['content']
+      tm = time.localtime()
+      critic_review_list = list(db.critic_review.find({}, {'_id': False}))
+      count = len(critic_review_list) + 1
+      upload_time = f'{tm.tm_year}-{tm.tm_mon}-{tm.tm_mday} {tm.tm_hour}:{tm.tm_min}:{tm.tm_sec}({wday[tm.tm_wday]})'
+      commenter = 'name' #'세션 호출을 통해서 받은 토큰이든 usernum이든을 통해서 name 호출(전문가버전이니 name 호출)'
       doc={
+         'no': count,
          'title':ogtitle,
          'image':ogimage,                 
          'comment': comment_receive,
-         'star':star_receive}
-      db.critics_review.insert_one(doc)
+         'star':star_receive,
+         'commenter':commenter,
+         'upload_time': upload_time}
+      db.critic_review.insert_one(doc)
       return jsonify({'code':200,'msg':'기록 완료!'})
    else :
-      return jsonify({'code':404,'msg':'게임 정보를 찾을 수 없었습니다.'})
+      return jsonify({'code':404,'msg':'잘못된 링크입니다.'})
 
 @app.route("/critic_review", methods=["GET"]) # POST 미적용 상태, 적용 전 확인은 db = client.dbsparta, client는 테스트용, games_data 값은 db.games로 변경 후 실행
 def game_get():
-   games_data = list(db.critics_review.find({},{'_id':False}))
-   print(games_data)
+   games_data = list(db.critic_review.find({},{'_id':False}))
    games_data.reverse()
-   print(games_data)
    return jsonify({'result':games_data, 'msg':'successed'})
+
 # ------평론가 평론 구현 (조영익) end----------------------------------------------------------------------------------------------------------------
 
 
@@ -80,3 +94,9 @@ def game_get():
 # app이라는 메인 함수 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
+    
+    
+    
+    
+    
+
