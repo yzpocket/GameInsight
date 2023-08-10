f5ac4bb9cd9a5aa079379c23795fe2eacc191414
INYONG KIM <ohnyong87@gmail.com>
2023-08-11 04:42:39 +0900

    feature : Add SignUp and SignIn function
    * 서버 500에러 해결
    - CORS 사용하여 요청 문제 보완
    - 모든 함수 재구성 흐름 읽기로 오류 원인 파악
    - append 되는 모든 값, name, id 체크(성민님 도움)
    - 주입되는 js 참조들 재정렬
    * 회원 가입 기능 추가
    - 회원 가입 양식을 통해 정보 획득
    - fetch를 통한 DB Insert
    * 로그인 기능 추가
    - 로그인 양식과 DB값 검증을 통한 로그인
    - Payload -> 토큰, 쿠키 생성, 사용자 정보 저장
    - 쿠키의 토큰을 디코딩하여 사용자 정보를 추출
    - Jinja2 if문을 통한 사용자 정보 활용
    -- 특정 유저 타입만의 서비스 이용(==관리자 유사)
    -- 로그인, 로그아웃 버튼 노출 상태 조절

--- app.py
+++ app.py
@@ -4,7 +4,11 @@
 # 요청 데이터에 접근 할 수 있는 flask.request 모듈
 # dictionary를 json형식의 응답 데이터를 내보낼 수 있는 jsonify 메서드
 from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+
 
 # MongoDB사용을 위한 pymongo와 certifi 임포트
 # MongoDB(Atlas Cloud)를 사용하기 위한 pymongo 임포트
@@ -46,24 +50,40 @@ import hashlib
 
 # ------------크롤링 PATH 부분----------------------------------------------------------------------------------------------------------------------
 
+
 # "localhost:5001/" URL요청에 메인 뷰 페이지 반환 응답
+# app.py (Flask 애플리케이션)
 @app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.user.find_one({"user_id": payload['user_id']})
-        return render_template('index.html', user_id=user_info["user_id"], user_name=user_info["user_name"], user_type=user_info["user_type"])
-    except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
-    except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+    if token_receive:
+        try:
+            payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+            user_info = db.user.find_one({"user_id": payload['user_id']})
+            return render_template('index.html', user_id=user_info["user_id"], user_name=user_info["user_name"], user_type=user_info["user_type"])
+        except jwt.ExpiredSignatureError:
+            return render_template('index.html', user_id=None, user_name=None, user_type=None)
+        except jwt.exceptions.DecodeError:
+            return render_template('index.html', user_id=None, user_name=None, user_type=None)
+    else:
+        return render_template('index.html', user_id=None, user_name=None, user_type=None)
+# @app.route('/')
+# def home():
+#     token_receive = request.cookies.get('mytoken')
+#     try:
+#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+#         user_info = db.user.find_one({"user_id": payload['user_id']})
+#         return render_template('index.html', user_id=user_info["user_id"], user_name=user_info["user_name"], user_type=user_info["user_type"])
+#     except jwt.ExpiredSignatureError:
+#         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+#     except jwt.exceptions.DecodeError:
+#         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
 # ------------기능 구현 함수 부분----------------------------------------------------------------------------------------------------------------------
 
 
 #################################
-##  HTML을 주는 부분             ##
+##  HTML을 주는 부분           ##
 #################################
 
 
@@ -72,14 +92,13 @@ def login():
     msg = request.args.get("msg")
     return render_template('login.html', msg=msg)
 
-
 @app.route('/register')
 def register():
     return render_template('register.html')
 
 
 #################################
-##  로그인을 위한 API            ##
+##  로그인을 위한 API          ##
 #################################
 
 # [회원가입 API]
@@ -112,7 +131,7 @@ def api_register():
 
 # [로그인 API]
 # id, pw를 받아서 맞춰보고, 토큰을 만들어 발급합니다.
-@app.route('/api/login', methods=['POST'])
+@app.route('/api/login', methods=['GET', 'POST'])
 def api_login():
     id_receive = request.form['id_give']
     pw_receive = request.form['pw_give']
@@ -134,7 +153,7 @@ def api_login():
             'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=200)
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-
+        
         # token을 줍니다.
         return jsonify({'result': 'success', 'token': token})
     # 찾지 못하면
@@ -146,7 +165,7 @@ def api_login():
 # 로그인된 유저만 call 할 수 있는 API입니다.
 # 유효한 토큰을 줘야 올바른 결과를 얻어갈 수 있습니다.
 # (그렇지 않으면 남의 장바구니라든가, 정보를 누구나 볼 수 있겠죠?)
-@app.route('/api/nick', methods=['GET'])
+@app.route('/api/logined', methods=['GET'])
 def api_valid():
     token_receive = request.cookies.get('mytoken')
 
--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,82 @@
 $(document).ready(function () {
-
+    const token = $.cookie('mytoken');
+    if (token) {
+        // 토큰이 존재하면 로그인한 상태로 간주하여 화면에 다른 내용을 표시할 수 있습니다.
+        // 이에 맞게 로그아웃 버튼을 보이게하고, 로그인 버튼을 숨깁니다.
+        $('#logout-button').show();
+        $('#login-button').hide();
+    } else {
+        // 토큰이 없는 경우는 로그아웃 버튼을 숨기고, 로그인 버튼을 보이게 합니다.
+        $('#logout-button').hide();
+        $('#login-button').show();
+    }
 });
+
+function show_register() {
+    // '/register'로 페이지 이동
+    window.location.href = '/register';
+}
+
+// 간단한 회원가입 함수입니다.
+// 아이디, 비밀번호, 닉네임을 받아 DB에 저장합니다.
+function register() {
+    $.ajax({
+        type: "POST",
+        url: "/api/register",
+        data: {
+            id_give: $('#user_id').val(),
+            pw_give: $('#user_password').val(),
+            email_give: $('#user_email').val(),
+            name_give: $('#user_name').val(),
+            post_give: $('#user_postcode').val(),
+            addr1_give: $('#user_address1').val(),
+            addr2_give: $('#user_address2').val(),
+            type_give: $('input[name=user_type]:checked').val()
+        },
+        success: function (response) {
+            if (response['result'] == 'success') {
+                alert('회원가입이 완료되었습니다.');
+                window.location.href = '/login';  // 회원가입 성공 시 로그인 페이지로 이동
+            } else {
+                alert(response['msg']);  // 실패 시 메시지 표시
+            }
+        }
+    });
+}
+
+
+// ['쿠키'라는 개념에 대해 알아봅시다]
+// 로그인을 구현하면, 반드시 쿠키라는 개념을 사용합니다.
+// 페이지에 관계없이 브라우저에 임시로 저장되는 정보입니다. 키:밸류 형태(딕셔너리 형태)로 저장됩니다.
+// 쿠키가 있기 때문에, 한번 로그인하면 네이버에서 다시 로그인할 필요가 없는 것입니다.
+// 브라우저를 닫으면 자동 삭제되게 하거나, 일정 시간이 지나면 삭제되게 할 수 있습니다.
+function login() {
+    $.ajax({
+        type: "POST",
+        url: "/api/login",
+        data: { id_give: $('#login_user_id').val(), pw_give: $('#login_user_password').val() },
+        success: function (response) {
+            if (response['result'] == 'success') {
+                // 로그인이 정상적으로 되면, 토큰을 받아옵니다.
+                // 이 토큰을 mytoken이라는 키 값으로 쿠키에 저장합니다.
+                $.cookie('mytoken', response['token'], { path: '/' });
+
+                alert('로그인 완료!')
+                window.location.href = '/'; // 또는 window.location.reload();
+            } else {
+                // 로그인이 안되면 에러메시지를 띄웁니다.
+                alert(response['msg'])
+            }
+        }
+    })
+}
+
+// 로그아웃 함수입니다.
+function logout() {
+    $.removeCookie('mytoken', { path: '/' });
+    alert('로그아웃 되었습니다.');
+    window.location.href = '/';
+}
+
+// 로그인 버튼 클릭 시 login 함수 호출
+$('#login-button').click(login);
