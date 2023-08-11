c490cd00622a069ea5274026d0ad7acdd3fca299
INYONG KIM <ohnyong87@gmail.com>
2023-08-11 20:49:04 +0900

    docs : Add publishing files
    * 배포 과정 파일 및 기타 수정사항 최종 반영

---
+++ deploy/application.py
@@ -0,0 +1,324 @@
+# 라이브러리 임포트
+# Flask Framework
+# view페이지 렌더링을 위한 render_template 메서드
+# 요청 데이터에 접근 할 수 있는 flask.request 모듈
+# dictionary를 json형식의 응답 데이터를 내보낼 수 있는 jsonify 메서드
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+from flask_cors import CORS
+import time
+wday = ['월', '화', '수', '목', '금', '토', '일']
+
+application = app = Flask(__name__)
+CORS(app)
+
+
+# MongoDB사용을 위한 pymongo와 certifi 임포트
+# MongoDB(Atlas Cloud)를 사용하기 위한 pymongo 임포트
+from pymongo import MongoClient
+import certifi
+# DB 커넥션 구성
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://ohnyong:test@cluster0.lu7mz8j.mongodb.net/',tlsCAFile=ca)
+db = client.gameinsight
+collection = db['game_rank']
+# 웹 크롤링을 위한 임포트
+import requests
+from bs4 import BeautifulSoup
+
+# JWT 토큰을 만들 때 필요한 비밀문자열입니다. 아무거나 입력해도 괜찮습니다.
+# 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 할 수 있습니다.
+SECRET_KEY = 'SPARTA'
+
+# JWT 패키지를 사용합니다. (설치해야할 패키지 이름: PyJWT)
+import jwt
+
+# 토큰에 만료시간을 줘야하기 때문에, datetime 모듈도 사용합니다.
+import datetime
+
+# 회원가입 시엔, 비밀번호를 암호화하여 DB에 저장해두는 게 좋습니다.
+# 그렇지 않으면, 개발자(=나)가 회원들의 비밀번호를 볼 수 있으니까요.^^;
+import hashlib
+
+
+# ------------크롤링 PATH 부분----------------------------------------------------------------------------------------------------------------------
+
+# 웹 크롤링 URL 지정과 requests를통한 데이터 가져오기->bs를 통한 파싱
+
+URL1 = "https://www.gamemeca.com/ranking.php"
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data1 = requests.get(URL1, headers=headers)
+soup1 = BeautifulSoup(data1.text, 'html.parser')
+URL5 = "https://www.gamemeca.com/ranking.php" 
+data = requests.get(URL5, headers=headers)
+
+# URL = "https://kworb.net/spotify/country/us_daily.html"
+# headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+# data = requests.get(URL, headers=headers)
+# soup = BeautifulSoup(data.text, 'html.parser')
+
+
+@app.route("/game_ranking", methods=["GET"])
+def game_rank_get():
+    all_game_ranks = list(db.game_rank.find({},{'_id':False}))
+    return jsonify({'result':all_game_ranks})
+
+
+# ------------크롤링 PATH 부분----------------------------------------------------------------------------------------------------------------------
+
+
+# "localhost:5001/" URL요청에 메인 뷰 페이지 반환 응답
+# app.py (Flask 애플리케이션)
+@app.route('/')
+def home():
+    token_receive = request.cookies.get('mytoken')
+    if token_receive:
+        try:
+            payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+            user_info = db.user.find_one({"user_id": payload['user_id']})
+            return render_template('index.html', user_id=user_info["user_id"], user_name=user_info["user_name"], user_type=user_info["user_type"])
+        except jwt.ExpiredSignatureError:
+            return render_template('index.html', user_id=None, user_name=None, user_type=None)
+        except jwt.exceptions.DecodeError:
+            return render_template('index.html', user_id=None, user_name=None, user_type=None)
+    else:
+        return render_template('index.html', user_id=None, user_name=None, user_type=None)
+# @app.route('/')
+# def home():
+#     token_receive = request.cookies.get('mytoken')
+#     try:
+#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+#         user_info = db.user.find_one({"user_id": payload['user_id']})
+#         return render_template('index.html', user_id=user_info["user_id"], user_name=user_info["user_name"], user_type=user_info["user_type"])
+#     except jwt.ExpiredSignatureError:
+#         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+#     except jwt.exceptions.DecodeError:
+#         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+# ------------기능 구현 함수 부분----------------------------------------------------------------------------------------------------------------------
+
+# user_review 페이지 호출
+@app.route('/user_review')
+def ur():
+   return render_template('user_review.html')
+
+#################################
+##  HTML을 주는 부분           ##
+#################################
+
+
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+@app.route('/mainsection')
+def mainsection():
+    return render_template('mainsection.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+#################################
+##  로그인을 위한 API          ##
+#################################
+
+# [회원가입 API]
+# id, pw, nickname을 받아서, mongoDB에 저장합니다.
+# 저장하기 전에, pw를 sha256 방법(=단방향 암호화. 풀어볼 수 없음)으로 암호화해서 저장합니다.
+@app.route('/api/register', methods=['POST'])
+def api_register():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+    email_receive = request.form['email_give']
+    name_receive = request.form['name_give']
+    post_receive = request.form['post_give']
+    addr1_receive = request.form['addr1_give']
+    addr2_receive = request.form['addr2_give']
+    type_receive = request.form['type_give']
+
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+    print(id_receive)
+    db.user.insert_one({'user_id': id_receive,
+                        'user_password': pw_hash,
+                        'user_email': email_receive,
+                        'user_name': name_receive,
+                        'user_postcode': post_receive,
+                        'user_address1': addr1_receive,
+                        'user_address2': addr2_receive,
+                        'user_type': type_receive})
+
+    return jsonify({'result': 'success'})
+
+
+# [로그인 API]
+# id, pw를 받아서 맞춰보고, 토큰을 만들어 발급합니다.
+@app.route('/api/login', methods=['GET', 'POST'])
+def api_login():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+
+    # 회원가입 때와 같은 방법으로 pw를 암호화합니다.
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    # id, 암호화된pw을 가지고 해당 유저를 찾습니다.
+    result = db.user.find_one({'user_id': id_receive, 'user_password': pw_hash})
+
+    # 찾으면 JWT 토큰을 만들어 발급합니다.
+    if result is not None:
+        # JWT 토큰에는, payload와 시크릿키가 필요합니다.
+        # 시크릿키가 있어야 토큰을 디코딩(=풀기) 해서 payload 값을 볼 수 있습니다.
+        # 아래에선 id와 exp를 담았습니다. 즉, JWT 토큰을 풀면 유저ID 값을 알 수 있습니다.
+        # exp에는 만료시간을 넣어줍니다. 만료시간이 지나면, 시크릿키로 토큰을 풀 때 만료되었다고 에러가 납니다.
+        payload = {
+            'user_id': id_receive,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=200)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+        
+        # token을 줍니다.
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+# [유저 정보 확인 API]
+# 로그인된 유저만 call 할 수 있는 API입니다.
+# 유효한 토큰을 줘야 올바른 결과를 얻어갈 수 있습니다.
+# (그렇지 않으면 남의 장바구니라든가, 정보를 누구나 볼 수 있겠죠?)
+@app.route('/api/logined', methods=['GET'])
+def api_valid():
+    token_receive = request.cookies.get('mytoken')
+
+    # try / catch 문?
+    # try 아래를 실행했다가, 에러가 있으면 except 구분으로 가란 얘기입니다.
+
+    try:
+        # token을 시크릿키로 디코딩합니다.
+        # 보실 수 있도록 payload를 print 해두었습니다. 우리가 로그인 시 넣은 그 payload와 같은 것이 나옵니다.
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        print(payload)
+
+        # payload 안에 id가 들어있습니다. 이 id로 유저정보를 찾습니다.
+        # 여기에선 그 예로 닉네임을 보내주겠습니다.
+        userinfo = db.user.find_one({'user_id': payload['user_id']}, {'_id': 0})
+        return jsonify({'result': 'success', 'user_id': userinfo['user_id'], 'user_name': userinfo['user_name'],  'user_type': userinfo['user_type']})
+    except jwt.ExpiredSignatureError:
+        # 위를 실행했는데 만료시간이 지났으면 에러가 납니다.
+        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
+    except jwt.exceptions.DecodeError:
+        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
+
+# ------평론가 평론 구현 (조영익) start----------------------------------------------------------------------------------------------------------------
+@app.route("/critic_review", methods=["POST"]) # html에는 미적용
+def game_post():
+    url_receive = request.form['url_give']
+    comment_receive=request.form['comment_give']
+    star_receive=request.form['star_give']
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive,headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+    search_url=soup.select_one('meta[property="og:url"]').find('https://www.gamemeca.com/game.php?rts=gmview&')
+    token_receive = request.cookies.get('mytoken')
+    if token_receive:
+        try:
+            payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+            user_info = db.user.find_one({"user_id": payload['user_id']})
+            commenter = user_info["user_name"]
+        except jwt.ExpiredSignatureError:
+            return jsonify({'code':-1,'msg':'로그인 정보가 만료되었습니다. 로그아웃 후 다시 로그인 해 주세요'})
+        except jwt.exceptions.DecodeError:
+            return jsonify({'code':-1,'msg':'로그인 정보를 확인할 수 없습니다.'})
+    
+    if search_url == -1:
+        return jsonify({'code':-1,'msg':'잘못된 링크입니다. 우측의 게임메타 링크를 통해 게임을 찾아보세요!'})
+    if star_receive=='-- 선택하기 --':
+        return jsonify({'code':-1,'msg':'별점을 선택해 주세요!'})
+    if comment_receive.replace(" ", '').replace('','') == '':
+        return jsonify({'code':-1,'msg':'리뷰를 작성해 주세요!'})
+    
+    ogtitle = soup.select_one('meta[property="og:title"]')['content'].split(" - ")[0]
+    ogimage=soup.select_one('meta[property="og:image"]')['content']
+    tm = time.localtime()
+    critic_review_list = list(db.critic_review.find({}, {'_id': False}))
+    count = len(critic_review_list) + 1
+    upload_time = f'{tm.tm_year}-{tm.tm_mon}-{tm.tm_mday} {tm.tm_hour}:{tm.tm_min}:{tm.tm_sec}({wday[tm.tm_wday]})'
+    
+
+    doc={
+        'no': count,
+        'title':ogtitle,
+        'image':ogimage,                 
+        'comment': comment_receive,
+        'star':star_receive,
+        'commenter':commenter,
+        'upload_time': upload_time}
+    db.critic_review.insert_one(doc)
+    return jsonify({'code':200,'msg':'기록 완료!'})
+
+
+@app.route("/critic_review", methods=["GET"]) # POST 미적용 상태, 적용 전 확인은 db = client.dbsparta, client는 테스트용, games_data 값은 db.games로 변경 후 실행
+def game_get():
+   games_data = list(db.critic_review.find({},{'_id':False}))
+   games_data.reverse()
+   return jsonify({'result':games_data, 'msg':'successed'})
+
+# ------평론가 평론 구현 (조영익) end----------------------------------------------------------------------------------------------------------------
+
+#유저리뷰에 쓰일 top50 게임목록 db에 등록 및 가져오기
+@app.route("/user_review_rank", methods=["GET"])
+def user_review_get():
+
+    games = soup1.select('#content > div.ranking_list > div.rank-list > div.content-left > table > tbody > tr')
+    db.user_review_games.delete_many({})
+    for a in games :
+        num = a.select_one('td:nth-child(1)').text[0:4].strip()
+        name = a.select_one('td:nth-child(4) > div.game-name > a').text
+        link = a.select_one('td:nth-child(2) > img')['src']
+        doc = {'num':num,'name':name,'link':link}
+        db.user_review_games.insert_one(doc)
+    
+    all_games = list(db.user_review_games.find({},{'_id':False}))
+    return jsonify({'result': all_games})
+
+#유저리뷰 가져오기
+@app.route("/user_review2", methods=["GET"])
+def user_review_get2():
+
+    all_user_reviews = list(db.user_review.find({},{'_id':False}))
+    return jsonify({'result': all_user_reviews})
+
+#유저리뷰 등록
+@app.route("/user_review_save", methods=["POST"])
+def save_user_review():
+    
+    gamename = request.form['gamename_give']    
+    starnum = request.form['starnum_give']
+    review = request.form['review_give']
+    today = request.form['today_give']
+
+    tmp = db.user_review_games.find_one({'name':gamename})
+    imgurl = tmp['link'] 
+   
+    doc = {'gamename':gamename,
+           'starnum':starnum,
+           'review':review,
+           'today':today,
+           'imgurl':imgurl}
+    db.user_review.insert_one(doc)
+    return jsonify({'msg': '저장 완료!'})
+
+
+# ------------기능 구현 함수 부분------------------------------------------------------------------------------------------------------------------------
+
+# app이라는 메인 함수 
+if __name__ == '__main__':
+    app.run()
+    
+    
+    
+    
+
---
+++ deploy/static/js/script.js
@@ -0,0 +1,293 @@
+$(document).ready(function () {
+    const token = $.cookie('mytoken');
+    critic_button_showing();
+    if (token) {
+        // 토큰이 존재하면 로그인한 상태로 간주하여 화면에 다른 내용을 표시할 수 있습니다.
+        // 이에 맞게 로그아웃 버튼을 보이게하고, 로그인 버튼을 숨깁니다.
+        $('#logout-button').show();
+        $('#login-button').hide();
+        $('#join-button').hide();
+    } else {
+        // 토큰이 없는 경우는 로그아웃 버튼을 숨기고, 로그인 버튼을 보이게 합니다.
+        $('#logout-button').hide();
+        $('#login-button').show();
+    }
+    listing()
+});
+
+function show_register() {
+    // '/register'로 페이지 이동
+    window.location.href = '/register';
+}
+
+// 간단한 회원가입 함수입니다.
+// 아이디, 비밀번호, 닉네임을 받아 DB에 저장합니다.
+function register() {
+    $.ajax({
+        type: "POST",
+        url: "/api/register",
+        data: {
+            id_give: $('#user_id').val(),
+            pw_give: $('#user_password').val(),
+            email_give: $('#user_email').val(),
+            name_give: $('#user_name').val(),
+            post_give: $('#user_postcode').val(),
+            addr1_give: $('#user_address1').val(),
+            addr2_give: $('#user_address2').val(),
+            type_give: $('input[name=user_type]:checked').val()
+        },
+        success: function (response) {
+            if (response['result'] == 'success') {
+                alert('회원가입이 완료되었습니다.');
+                window.location.href = '/';  // 회원가입 성공 시 로그인 페이지로 이동
+            } else {
+                alert(response['msg']);  // 실패 시 메시지 표시
+            }
+        }
+    });
+}
+
+
+// ['쿠키'라는 개념에 대해 알아봅시다]
+// 로그인을 구현하면, 반드시 쿠키라는 개념을 사용합니다.
+// 페이지에 관계없이 브라우저에 임시로 저장되는 정보입니다. 키:밸류 형태(딕셔너리 형태)로 저장됩니다.
+// 쿠키가 있기 때문에, 한번 로그인하면 네이버에서 다시 로그인할 필요가 없는 것입니다.
+// 브라우저를 닫으면 자동 삭제되게 하거나, 일정 시간이 지나면 삭제되게 할 수 있습니다.
+function login() {
+    $.ajax({
+        type: "POST",
+        url: "/api/login",
+        data: { id_give: $('#login_user_id').val(), pw_give: $('#login_user_password').val() },
+        success: function (response) {
+            if (response['result'] == 'success') {
+                // 로그인이 정상적으로 되면, 토큰을 받아옵니다.
+                // 이 토큰을 mytoken이라는 키 값으로 쿠키에 저장합니다.
+                $.cookie('mytoken', response['token'], { path: '/' });
+
+                alert('로그인 완료!')
+                window.location.href = '/'; // 또는 window.location.reload();
+            } else {
+                // 로그인이 안되면 에러메시지를 띄웁니다.
+                alert(response['msg'])
+            }
+        }
+    })
+}
+
+// 로그아웃 함수입니다.
+function logout() {
+    $.removeCookie('mytoken', { path: '/' });
+    alert('로그아웃 되었습니다.');
+    window.location.href = '/';
+}
+
+// 로그인 버튼 클릭 시 login 함수 호출
+$('#login-button').click(login);
+
+function execPostCode() {
+    new daum.Postcode({
+        oncomplete: function(data) {
+           // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+           // 도로명 주소의 노출 규칙에 따라 주소를 조합한다.
+           // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+           var fullRoadAddr = data.roadAddress; // 도로명 주소 변수
+           var extraRoadAddr = ''; // 도로명 조합형 주소 변수
+
+           // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+           // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+           if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+               extraRoadAddr += data.bname;
+           }
+           // 건물명이 있고, 공동주택일 경우 추가한다.
+           if(data.buildingName !== '' && data.apartment === 'Y'){
+              extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+           }
+           // 도로명, 지번 조합형 주소가 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+           if(extraRoadAddr !== ''){
+               extraRoadAddr = ' (' + extraRoadAddr + ')';
+           }
+           // 도로명, 지번 주소의 유무에 따라 해당 조합형 주소를 추가한다.
+           if(fullRoadAddr !== ''){
+               fullRoadAddr += extraRoadAddr;
+           }
+
+           // 우편번호와 주소 정보를 해당 필드에 넣는다.
+           console.log(data.zonecode);
+           console.log(fullRoadAddr);
+           
+           
+           
+           $("[name=user_postcode]").val(data.zonecode);
+           $("[name=user_address1]").val(fullRoadAddr);
+           document.getElementById("user_address2").focus();
+           /* document.getElementById('signUpUserPostNo').value = data.zonecode; //5자리 새우편번호 사용
+           document.getElementById('signUpUserCompanyAddress').value = fullRoadAddr;
+           document.getElementById('signUpUserCompanyAddressDetail').value = data.jibunAddress; */
+       }
+    }).open();
+}
+
+
+
+function critic_review_open(x){
+  $('#critic_'+x).removeClass('ellipsis')
+  $('#more_'+x).empty()
+  tmp_html = `<a href="#" onclick="critic_review_close('${x}')">닫기</a>`
+  $('#more_'+x).append(tmp_html)
+}
+
+function critic_review_close(x){
+  $('#critic_'+x).addClass('ellipsis')
+  $('#more_'+x).empty()
+  tmp_html = `<a href="#" onclick="critic_review_open('${x}')">더보기</a>`
+  $('#more_'+x).append(tmp_html)
+}
+
+function critic_listing() {
+    fetch('/critic_review')
+      .then((res) => res.json())
+      .then((data) => {
+        let rows = data['result']
+        $('#section').empty()
+        $('#section').append(`<div class="col row-cols-4 row-cols-md-1 g-0 overflow-auto" id="cards-box">`)
+        rows.forEach((a)=>{
+              tmp_html = `<div class="card mb-3" style="max-height: auto;">
+                <div class="row row-cols-2 g-0">
+                  <div class="col-md-3 imagemargin" style="max-width: 135px;">
+                    <img src="${a['image']}" class="img-fluid rounded-start" >
+                  </div>
+                  <div class="col-md-9">
+                    <div class="card-body">
+                      <h5 class="card-title">${a['title']}</h5>
+                      <p class="card-text">${'★'.repeat(a['star']) + '☆'.repeat(5-a['star']) + ' ('+ a['star']+'/5)'} | -${a['commenter']}</p>
+                      <pre align="justify" class="card-text ellipsis"  id="critic_${a['no']}">${a['comment']} </pre>
+                      <div class="row" id = "more_${a['no']}">
+                        <a href="#" onclick="critic_review_open('${a['no']}')">더보기</a>
+                      </div>
+                      <p class="card-text"style="float:right"><small class="text-muted">${a['upload_time']}</small></p>
+                    </div>
+                  </div>
+                </div>`
+            $('#cards-box').append(tmp_html)
+        })
+      })
+}
+
+function critic_showing() {
+  $('#section').empty()
+  tmp_html = `
+  <div class="critic_review">
+    <h5 style="float:left;margin-left:10px;margin-bottom:5px">게임 URL
+        <label>
+            <span style="float:left;margin-left:10px">
+                <a href="https://www.gamemeca.com/game.php" target="_blank">게임메카 링크</a>
+            </span>
+        </label>
+    </h5>
+    <div class="critic_review_block" >
+        <input id="critic_url" type="email" class="form-control" placeholder="게임 메카에서 게임을 찾아보세요!">  
+    </div>
+    <h5 style="float:left;margin-left:10px;margin-bottom:5px"><label for="inputGroupSelect01">별점</label></h5>
+    <div class="input-group critic_review_block" >
+        <select class="form-select" id="critic_star">
+            <option selected>-- 선택하기 --</option>
+            <option value="1">⭐</option>
+            <option value="2">⭐⭐</option>
+            <option value="3">⭐⭐⭐</option>
+            <option value="4">⭐⭐⭐⭐</option>
+            <option value="5">⭐⭐⭐⭐⭐</option>
+        </select>
+    </div>
+    <h5 style="float:left;margin-left:10px;margin-bottom:5px"><label for="floatingTextarea2">리뷰</label></h5>
+    <div class="critic_review_block">
+        <textarea id="critic_comment" class="form-control" style = "height:200px;" placeholder="리뷰를 남겨주세요!"></textarea>
+        
+    </div>
+    
+    <div class="mybtns">
+        <button onclick="close_box()" type="button" class="btn btn-dark">기록하기</button>
+    </div>
+</div>`
+$('#section').append(tmp_html)
+}
+
+function critic_posting() {
+  let formData = new FormData()
+  let url = $('#critic_url').val()
+  let comment = $('#critic_comment').val()
+  let star = $('#critic_star').val()
+  formData.append('url_give', url)
+  formData.append('comment_give', comment)
+  formData.append('star_give', star)
+  fetch('/critic_review', { method: 'POST', body: formData })
+      .then((res) => res.json())
+      .then((data) => {
+        if(data['code']==-1){
+          alert(data['msg'])
+        }
+        else if(data['code']==200){
+          alert('저장되었습니다.')
+          window.location.reload()
+        }
+          
+      })
+}
+
+function close_box(){       
+  var returnValue = confirm('저장하시겠습니까? 한 번 저장한 리뷰는 수정 및 삭제가 불가능합니다.')
+  if (returnValue){
+      critic_posting()
+  }
+  else{
+      alert('취소되었습니다.')}
+}
+
+function critic_button_showing(){
+  fetch('/api/logined',{method:'GET',})
+  .then((res) => res.json())
+  .then((data) => {
+    if (data['result']=='success' && data['user_type'] == 1){
+      $('#critic-button').show()
+    }
+    else{
+      $('#critic-button').hide()
+    }
+  })
+}
+
+function listing() {
+    fetch('/game_ranking').then((res) => res.json()).then((data) => {
+        let rows = data['result']
+        $('#game-rank').empty()
+        rows.forEach(a => {
+            let rank = a['rank']
+            let icon_url = a['icon_url']
+            let name = a['name']
+            let company = a['company']
+            let genre = a['genre']
+            let pay = a['pay']
+            
+            let temp_html = `<tr class="ranking-table-rows">
+                                <td>
+                                    <id="game_rank"> <span class="rank red">${rank}</span>
+                                </td>
+                                <td>
+                                    <img id="game_icon" class="game-icon" src="${icon_url}">
+                                </td>
+                                <td>
+                                    <div id="game_name" class="game-name">${name}</a></div>
+                                </td>
+                                <td>
+                                    <div class="game-info">
+                                        <p id="game_company" class="company">${company}</a></p>
+                                        <span id="game_genre">${genre}</span> |
+                                        <span id="game_pay">${pay}</span>
+                                    </div>
+                                </td>
+                            </tr>`
+            $('#game-rank').append(temp_html)
+        })
+    })
+}
+
